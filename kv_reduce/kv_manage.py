1edc24dce3f10f6ac5b89af3b6a9a932277a0e51
nn <YOUR_EMAIL>
2023-06-29 07:24:37 +0000

    update

--- kv_reduce/kv_manage.py
+++ kv_reduce/kv_manage.py
@@ -1,6 +1,9 @@
 import torch
 import torch.nn as nn
 from transformers.models.llama.modeling_llama import _make_causal_mask
+from torch import Tensor
+
+from typing import Tuple
 
 
 def replace_modules(model: nn.Module, module_map={}):
@@ -66,6 +69,56 @@ def greedy_generate_mask(attention: torch.Tensor,
         mask[:, :, i, :] = mask_i
 
     return mask
+
+
+class KvManager:
+
+    def __init__(self, num_kv=10, group=1, decay=0.99, recent=10) -> None:
+        self.reset(num_kv, group, decay, recent)
+
+    def reset(self, num_kv=10, group=1, decay=0.99, recent=10):
+        self.num_kv = num_kv
+        self.group = group
+        self.decay = decay
+        self.recent = recent
+        self.imp: torch.Tensor = None
+
+        self.passed_kv = 0
+
+    def step(self, attn_weights: Tensor, past_key_value: Tuple[Tensor,
+                                                               Tensor]):
+        max_inf = torch.finfo(attn_weights.dtype).max
+        B, He, N, _ = attn_weights.shape
+        assert N == 1
+        if self.imp is None:
+            self.imp = attn_weights.clone() * (1 - self.decay)
+        else:
+            # self.imp: B He 1 KV-1
+            self.imp = torch.cat(
+                [self.imp,
+                 torch.zeros(B, He, 1, 1).to(self.imp.device)],
+                dim=-1)
+
+            self.imp = self.imp * self.decay + attn_weights * (1 - self.decay)
+        num_kv = past_key_value[0].shape[-2]
+        if past_key_value is not None and num_kv > self.num_kv:
+            imp = self.imp.clone()
+            imp[-self.recent:] = max_inf
+            index: torch.Tensor = torch.topk(imp,
+                                             self.num_kv,
+                                             dim=-1,
+                                             largest=True)[1]
+
+            self.imp = self.imp.gather(-1, index)
+            index = index.squeeze(-2).unsqueeze(-1)
+            index = index.expand(
+                [*index.shape[:-1], past_key_value[0].shape[-1]])
+            past_key_value = (
+                past_key_value[0].gather(-2, index),
+                past_key_value[1].gather(-2, index),
+            )
+        self.passed_kv += 1
+        return past_key_value
 
 
 if __name__ == '__main__':
--- kv_reduce/model.py
+++ kv_reduce/model.py
@@ -8,7 +8,7 @@ import torch.utils.checkpoint
 from torch import nn
 from torch.nn import BCEWithLogitsLoss, CrossEntropyLoss, MSELoss
 # from kv_manage import generate_mask
-from kv_reduce.kv_manage import generate_mask, greedy_generate_mask
+from kv_reduce.kv_manage import generate_mask, greedy_generate_mask, KvManager
 
 
 class KvLlamaAttention(LlamaAttention):
@@ -61,12 +61,14 @@ class KvLlamaAttention(LlamaAttention):
                 f"Attention weights should be of size {(bsz, self.num_heads, q_len, kv_seq_len)}, but is"
                 f" {attn_weights.size()}")
 
-        if self.num_kv != -1:
+        #####################################################################################################
+        if self.num_kv != -1 and use_cache == False:
             attention_mask = greedy_generate_mask(attn_weights,
                                                   attention_mask,
                                                   remain_kv=self.num_kv,
                                                   recent=10,
                                                   group=self.group)
+        #####################################################################################################
 
         if attention_mask is not None:
             # if attention_mask.size() != (bsz, 1, q_len, kv_seq_len):
@@ -105,3 +107,114 @@ class KvLlamaAttention(LlamaAttention):
         new_module = cls(module.config)
         new_module.load_state_dict(module.state_dict())
         return new_module
+
+
+class KvLlamaAttentionForGenerate(LlamaAttention):
+    """Multi-headed attention from 'Attention Is All You Need' paper"""
+
+    def __init__(self, config: LlamaConfig):
+        super().__init__(config)
+        self.kv_manager = KvManager()
+
+    def forward(
+        self,
+        hidden_states: torch.Tensor,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        past_key_value: Optional[Tuple[torch.Tensor]] = None,
+        output_attentions: bool = False,
+        use_cache: bool = False,
+    ) -> Tuple[torch.Tensor, Optional[torch.Tensor],
+               Optional[Tuple[torch.Tensor]]]:
+        bsz, q_len, _ = hidden_states.size()
+
+        query_states = self.q_proj(hidden_states).view(
+            bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
+        key_states = self.k_proj(hidden_states).view(
+            bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
+        value_states = self.v_proj(hidden_states).view(
+            bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
+
+        kv_seq_len = key_states.shape[-2]
+        if past_key_value is not None:
+            kv_seq_len += self.kv_manager.passed_kv
+        cos, sin = self.rotary_emb(value_states, seq_len=kv_seq_len)
+        query_states, key_states = apply_rotary_pos_emb(
+            query_states, key_states, cos, sin, position_ids)
+        # [bsz, nh, t, hd]
+
+        if past_key_value is not None:
+            # reuse k, v, self_attention
+            key_states = torch.cat([past_key_value[0], key_states], dim=2)
+            value_states = torch.cat([past_key_value[1], value_states], dim=2)
+
+        past_key_value = (key_states, value_states) if use_cache else None
+
+        attn_weights = torch.matmul(query_states, key_states.transpose(
+            2, 3)) / math.sqrt(self.head_dim)
+
+        # if attn_weights.size() != (bsz, self.num_heads, q_len, kv_seq_len):
+        #     raise ValueError(
+        #         f"Attention weights should be of size {(bsz, self.num_heads, q_len, kv_seq_len)}, but is"
+        #         f" {attn_weights.size()}")
+
+        if attention_mask is not None:
+            if attention_mask.size() != (bsz, 1, q_len, kv_seq_len):
+                raise ValueError(
+                    f"Attention mask should be of size {(bsz, 1, q_len, kv_seq_len)}, but is {attention_mask.size()}"
+                )
+            attn_weights = attn_weights + attention_mask
+            attn_weights = torch.max(
+                attn_weights,
+                torch.tensor(torch.finfo(attn_weights.dtype).min))
+
+        # upcast attention to fp32
+        attn_weights = nn.functional.softmax(attn_weights,
+                                             dim=-1,
+                                             dtype=torch.float32).to(
+                                                 query_states.dtype)
+        attn_output = torch.matmul(attn_weights, value_states)
+
+        if attn_output.size() != (bsz, self.num_heads, q_len, self.head_dim):
+            raise ValueError(
+                f"`attn_output` should be of size {(bsz, self.num_heads, q_len, self.head_dim)}, but is"
+                f" {attn_output.size()}")
+
+        attn_output = attn_output.transpose(1, 2)
+        attn_output = attn_output.reshape(bsz, q_len, self.hidden_size)
+
+        attn_output = self.o_proj(attn_output)
+
+        past_key_value = self.kv_manager.step(attn_weights, past_key_value)
+
+        if not output_attentions:
+            attn_weights = None
+
+        return attn_output, attn_weights, past_key_value
+
+
+if __name__ == '__main__':
+    from transformers.models.llama.modeling_llama import LlamaConfig
+
+    B, N, C = 2, 10, 512
+    h = 32
+    He = C // h
+
+    config = LlamaConfig(hidden_size=512, num_attention_heads=He)
+    attention = KvLlamaAttentionForGenerate(config)
+    attention.kv_manager.reset(5)
+    attention.kv_manager.passed_kv = 10
+
+    past_key_value = (
+        torch.randn([B, He, N, h]),
+        torch.randn(B, He, N, h),
+    )
+    for i in range(5):
+        out = attention.forward(
+            torch.randn(2, 1, 512),
+            past_key_value=past_key_value,
+            position_ids=torch.tensor([10 + i]),
+            use_cache=True,
+        )
+        _, _, past_key_value = out
+        print(past_key_value[0].shape, past_key_value[1].shape)
